e083a7b7f770367d26b9642b77a132c561901a17
nhihnguyen <70934146+nhihnguyen@users.noreply.github.com>
2023-04-03 22:05:37 -0700

    add site-leaderboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteLeaderboardComponent } from './site-leaderboard/site-leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteLeaderboardComponent
   ],
   imports: [
     BrowserModule,
