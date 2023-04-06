47c06871dbc9bdfa699a79dc82f8d37fc66f6ee6
Cole-Barazoto <colebarazoto@gmail.com>
2023-04-05 20:42:02 -0700

    single photo, like, comment components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,9 @@ import { SiteLeaderboardComponent } from './site-leaderboard/site-leaderboard.co
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
+import { SinglePhotoComponent } from './single-photo/single-photo.component';
+import { LikesDialogComponent } from './likes-dialog/likes-dialog.component';
+import { AddCommentComponent } from './add-comment/add-comment.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +19,16 @@ import { LoginComponent } from './login/login.component';
     SiteLeaderboardComponent,
     NavigationBarComponent,
     SignUpComponent,
-    LoginComponent
+    LoginComponent,
+    SinglePhotoComponent,
+    LikesDialogComponent,
+    AddCommentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
