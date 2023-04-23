df98ff5c9685c66876dc223cfcd677cf357bc3ba
Cole-Barazoto <colebarazoto@gmail.com>
2023-04-22 17:21:00 -0700

    nav bar and routing for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'social-media-app';
+
+  userLoggedIn = false;
+
+  constructor(private cookie: CookieService,
+              private internalService: DataService) {}
+
+  ngOnInit() {
+    // if(this.cookie.get('user_id') != '') {
+    //     this.userLoggedIn = true;
+    // }
+    this.internalService.getData().subscribe(data => {
+        this.userLoggedIn = data;
+    })
+  }
+
+//   loginEvent(loginMessage: string) {
+//     loginMessage === "loggedin" ? this.userLoggedIn = true : this.userLoggedIn = false;
+//   }
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataService {
+        private data = new BehaviorSubject<boolean>(false);
+  
+    getData() {
+        return this.data.asObservable();
+    }
+  
+    setData(data: boolean) {
+        this.data.next(data);
+    }
 }
