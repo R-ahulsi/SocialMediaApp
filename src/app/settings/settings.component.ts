94fa27e1b6f2ddf9f77413962251d474b2c9ad41
unknown <rahulphx@gmail.com>
2023-04-23 16:59:37 -0700

    hashed passwords for settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { SocialMediaService } from 'app/social-media.service';
+import * as bcrypt from 'bcryptjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -17,11 +19,21 @@ export class SettingsComponent {
   };
 
   genders = ['M', 'F'];
+  hashedPassword = '';
 
-  constructor(private socialMediaService: SocialMediaService) {}
+  constructor(private socialMediaService: SocialMediaService,
+              private router: Router) {}
   
   onSubmit() {
-    this.socialMediaService.updateUserInfo(this.model.change_fname, this.model.change_lname, this.model.password, 
-                                            this.model.hometown, this.model.dob, this.model.gender);
+    bcrypt.hash(this.model.password, 10, (err, hash) => {
+      if (err) {
+        console.error(err);
+      } else {
+        this.model.password = hash;
+        this.socialMediaService.updateUserInfo(this.model.change_fname, this.model.change_lname, this.model.password, 
+                                                this.model.hometown, this.model.dob, this.model.gender);
+        this.router.navigate(['/profile']);
+      }
+    });
   }
 }
